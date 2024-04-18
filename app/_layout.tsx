69f126b3408bf7b1a5fce3524c2d25c9fb4273da
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-04-18 06:44:15 +0300

    install packages, complete home screen

---
+++ app/_layout.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { StatusBar } from "expo-status-bar";
+
+export const unstable_settings = {
+  initialRouteName: "(tabs)",
+};
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          statusBarTranslucent: true,
+          statusBarColor: "transparent",
+          navigationBarColor: "transparent",
+        }}
+      />
+    </Stack>
+  );
+}
