872a65a4c39d9d04ef1fd287010dcd43a565cd5f
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-04-24 03:25:07 +0300

    install zustand,  mmkv packages and implement zustand store with mmkv

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,54 +1,35 @@
 import { StyleSheet } from "react-native";
 
 import * as DropdownMenu from "zeego/dropdown-menu";
+
 import RoundBtn from "./RoundBtn";
 
 const Dropdown = () => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <RoundBtn icon={"ellipsis-horizontal"} text={"More"} />
+        <RoundBtn icon="ellipsis-horizontal" text="More" />
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content>
         <DropdownMenu.Item key="statement">
           <DropdownMenu.ItemTitle>Statement</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIcon
-            ios={{
-              name: "list.bullet.rectangle.fill",
-              pointSize: 24,
-            }}
-          ></DropdownMenu.ItemIcon>
+          <DropdownMenu.ItemIcon androidIconName="ic_menu_sort_by_size"></DropdownMenu.ItemIcon>
         </DropdownMenu.Item>
 
         <DropdownMenu.Item key="converter">
           <DropdownMenu.ItemTitle>Converter</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIcon
-            ios={{
-              name: "coloncurrencysign.arrow.circlepath",
-              pointSize: 24,
-            }}
-          ></DropdownMenu.ItemIcon>
+          <DropdownMenu.ItemIcon androidIconName="ic_menu_rotate"></DropdownMenu.ItemIcon>
         </DropdownMenu.Item>
 
         <DropdownMenu.Item key="background">
           <DropdownMenu.ItemTitle>Background</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIcon
-            ios={{
-              name: "photo.fill",
-              pointSize: 24,
-            }}
-          ></DropdownMenu.ItemIcon>
+          <DropdownMenu.ItemIcon androidIconName="ic_menu_gallery"></DropdownMenu.ItemIcon>
         </DropdownMenu.Item>
 
         <DropdownMenu.Item key="account">
           <DropdownMenu.ItemTitle>Add new account</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemIcon
-            ios={{
-              name: "plus.rectangle.on.folder.fill",
-              pointSize: 24,
-            }}
-          ></DropdownMenu.ItemIcon>
+          <DropdownMenu.ItemIcon androidIconName="ic_menu_upload"></DropdownMenu.ItemIcon>
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
---
+++ store/balenceStore.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import { zustandStorage } from "./mmkv-storage";
+
+export interface Transaction {
+  id: string;
+  date: Date;
+  title: string;
+  amount: number;
+}
+
+export interface BalanceState {
+  transactions: Transaction[];
+  balance: () => number;
+  clearTransactions: () => void;
+  runTransaction: (transaction: Transaction) => void;
+}
+
+export const useBalanceStore = create<BalanceState>()(
+  persist(
+    (set, get) => ({
+      transactions: [],
+
+      balance: () => 0,
+
+      clearTransactions: () => {
+        set({ transactions: [] });
+      },
+
+      runTransaction: (transaction: Transaction) => {
+        set((state) => ({
+          transactions: [...state.transactions, transaction],
+        }));
+      },
+    }),
+
+    {
+      name: "balance",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+);
